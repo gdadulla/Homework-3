b91ca673f10a416ce46a7f4a00694bc35487c4b9
gdadulla <gdadulla@albany.edu>
2017-03-04 16:42:28 -0500

    Creates setName and getName methods

--- Node.java
+++ Node.java
@@ -5,8 +5,17 @@ public class Node {
 		private String name;
 		private Node next;
 		
- public Node(String name){
-	 this.name = name;
-	 this.next = null;
- }
+		public Node(String name){
+			this.name = name;
+			this.next = null;
+		}
+		
+		public void setName(String name){
+			this.name = name;
+		}
+		
+		public String getName(){
+			return name;
+		}
+ 
 }
